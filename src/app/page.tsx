9e6f15dfb4103d659c9742070e65c8ce20510ad0
Samuel Mason <me@sammason.dev>
2023-12-12 15:53:12 -0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
+import { Button } from '@nextui-org/react';
 
 export default function Home() {
-  return (
-    
-  )
+  return <div>
+    <Button>Click Here!</Button>
+  </div>;
 }
