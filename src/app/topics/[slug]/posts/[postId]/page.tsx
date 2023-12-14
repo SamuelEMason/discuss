7ad5cd6fa687025fd7babb27cc9dbbaaef968d6f
Samuel Mason <me@sammason.dev>
2023-12-14 15:12:09 -0500

    PostShowPage completed

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,8 +1,10 @@
-import Link from "next/link";
-import PostShow from "@/components/posts/post-show";
-import CommentList from "@/components/comments/comment-list";
-import CommentCreateForm from "@/components/comments/comment-create-form";
-import paths from "@/paths";
+import Link from 'next/link';
+import { Suspense } from 'react';
+import CommentList from '@/components/comments/comment-list';
+import CommentCreateForm from '@/components/comments/comment-create-form';
+import PostShow from '@/components/posts/post-show';
+import PostShowLoading from '@/components/posts/PostShowLoading';
+import paths from '@/paths';
 
 interface PostShowPageProps {
   params: {
@@ -17,11 +19,13 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
   return (
     <div className="space-y-3">
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
-        {"< "}Back to {slug}
+        {'< '}Back to {slug}
       </Link>
-      {/* <PostShow /> */}
-      {/* <CommentCreateForm postId={postId} startOpen /> */}
-      {/* <CommentList comments={comments} /> */}
+      <Suspense fallback={<PostShowLoading />}>
+        <PostShow postId={postId} />
+      </Suspense>
+      <CommentCreateForm postId={postId} startOpen />
+      <CommentList postId={postId} />
     </div>
   );
 }
